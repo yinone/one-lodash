ed565576cc6670ef1b916794480e09b79accb9e2
eleven <eleven.image@gmail.com>
2019-03-27 19:36:44 +0800

    update concat

--- src/concat/index.js
+++ src/concat/index.js
@@ -1,71 +1,18 @@
-function concat() {
-  const args = argumentsToArray(arguments)
-  
-  const length = args.length
-  const arr = args[0]
-  const baseArr = isArray(arr) ? arr : [arr]
-  const values = new Array(length ? length - 1 : 0)
-  let index = length
-  
-  while(index--) {
-    values[index - 1] = args[index]
-  }
-  
-  return length ? arrayPush(shallowCopy(baseArr), shallowFlat(values)) : []
+
+function isArguments(arg) {
+  return Object.prototype.toString.call(arg) === '[object Arguments]'
 }
 
-function shallowFlat(arr) {
-  if (!isArray(arr)) {
-    return []
-  }
-  
-  let result = []
-  let resIndex = 0
-  
-  while(resIndex < arr.length) {
-    result = arrayPush(result, arr[resIndex])
-    resIndex++
-  }
-  
-  return result
+function isArray(arr) {
+  return Object.prototype.toString.call(arr) === '[object Array]'
 }
 
-function shallowCopy(arr) {
-  if (!isArray(arr)) {
-    return []
-  }
-  
-  
-}
-
-function deepCopy(arr) {
-  
-//   const result = []
-  return arr
-}
-
-function arrayPush() {
-  const args = argumentsToArray(arguments)
-  
-  if (!isArray(args[0])) {
-    return []
-  }
-  
-  
-  const baseArr = deepCopy(args[0])
-  const pushArr = deepCopy(args[1])
-  
-  let index = 0
-  
-  while(index < pushArr.length) {
-    
-    baseArr[baseArr.length + index] = pushArr[index]
-    index++
-  }
-  
-  console.log(baseArr)
-  return baseArr
-}
+/**
+  * transform arguments obejct to array
+  *  @param {Object} Arguments Object
+  *  @returns {Array} result return `array`
+  *
+  **/
 
 function argumentsToArray(arg) {
   
@@ -82,28 +29,122 @@ function argumentsToArray(arg) {
     resIndex++
   }
   
-  console.log(result)
   return result
 }
 
-function isArguments(arg) {
-  return Object.prototype.toString.call(arg) === '[object Arguments]'
-}
 
-function isArray(arr) {
-  return Object.prototype.toString.call(arr) === '[object Array]'
-}
-
-function isArrayLike() {
+/**
+  *  flat array
+  *  @param {Array} arr The array to flat
+  *  @returns {Array} return `result`
+  *  @example
+  *  
+  *  const arr = [1,2,3, [4,5], [[2], 3]]
+  *  baseFlat(arr) => [1,2,3,4,5,[2],3]
+  *
+  */
+function baseFlat(arr) {
   
-}
-
-function isObject() {
+  if (!isArray(arr)) {
+    return []
+  }
   
-}
-
-function isObjectLike() {
+  let result = []
+  let resIndex = 0
   
+  while(resIndex < arr.length) {
+    
+    const values = arr[resIndex]
+    if (isArray(values)) {
+      result = arrayPush(result, values)
+      
+    } else {
+      
+      result[resIndex] = values
+    }
+   
+    resIndex = result.length
+  } 
+  
+  return result
 }
 
-shallowFlat([1,2])
+/**
+  *  append values to array
+  *  @param {Array} array The array to modify
+  *  @param {Array} values the values to append
+  *  @returns {Array} return `array`
+  */
+
+function arrayPush(array, values) {
+   
+  if (!isArray(array) || !isArray(values)) {
+    return []
+  }
+  
+  const length = values.length
+  const offsetIndex = array.length
+  let index = 0
+  
+  while(index < length) {
+    array[offsetIndex + index] = values[index]
+    index++
+  }
+  
+  return array
+}
+
+// arrayPush([], [1,2,3, [4], [[5]]])
+
+/**
+  *  shallow copy array
+  *  @param {Array} arr The array to copy
+  *  @returns {Array}  return the new array
+  *
+  **/
+
+function shallowCopy(arr) {
+  if (!isArray(arr)) {
+    return []
+  }
+  
+  const result = []
+  let resIndex = 0
+  
+  while(resIndex < arr.length) {
+    result[resIndex] = arr[resIndex]
+    resIndex++
+  }
+  return result
+}
+
+/**
+  *  concat array
+  *  @param {Array} array The array to concatenate
+  *  @param {*}  values The values to concatenate
+  *  @returns {Array} return the new array
+  *  @example
+  *  
+  *  concat([], 2, [3], [[2,5]]) => [2,5, [2,5]]
+  **/
+
+function concat() {
+  const args = argumentsToArray(arguments)
+  
+  const length = args.length
+  const arr = args[0]
+  const baseArr = isArray(arr) ? arr : [arr]
+  const value = new Array(length ? length - 1 : 0)
+  let index = length
+  
+  while(index--) {
+    value[index - 1] = args[index]
+  }
+  
+  return length ? arrayPush(shallowCopy(baseArr), baseFlat(value)) : []
+}
+
+exports.shallowCopy = shallowCopy
+exports.arrayPush = arrayPush
+exports.baseFlat= baseFlat
+exports.concat = concat

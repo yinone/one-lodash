d2735e018cde72f4158804660f33e512416294f7
eleven <eleven.image@gmail.com>
2019-04-18 23:56:29 +0800

    fixed test

--- test/Array/fill.test.js
+++ test/Array/fill.test.js
@@ -6,11 +6,15 @@ describe('fill', function() {
     it('should return the correct value', function() {
       const returnArray = _.fill([1,2,3,0], '*', 1, 2)
       const fillArray = _.fill([1,2,3,4], '*')
+      const anotherArr = _.fill([1,2,3,4], {name: 'eleven'}, 2, 1)
+
       returnArray.should.be.a('array')
       returnArray.should.have.lengthOf(4)
       returnArray.should.eql([1, '*', 3, 0])
 
       fillArray.should.eql(['*', '*', '*', '*'])
+
+      anotherArr.should.eql([])
     })
   })
 })
